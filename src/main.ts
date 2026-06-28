8279ac6842cc81a5336b1e78b5a57f67b9429bfa
Banele Ngubane <84304786+Banele-stack@users.noreply.github.com>
2026-06-28 14:18:33 +0200

    CORS Error again

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:3002",
-      "https://cosmorooms.banelengubane.dev",
+      "https://thecosmopolitan.banelengubane.dev",
     ],
     credentials: true,
   });
